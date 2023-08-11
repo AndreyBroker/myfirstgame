e9ff5a301895284ca33baf589a8213d147e11669
Andrey Broker <andreybroker02@gmail.com>
2023-08-11 14:40:33 -0300

    Finalização movimentações do personagem

--- main.py
+++ main.py
@@ -8,18 +8,24 @@ pygame.init()
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.sprites = []
-        self.sprites.append(pygame.transform.scale(pygame.image.load('./Images/Runing Animation/image1.png'), (200, 200)))
-        self.sprites.append(pygame.transform.scale(pygame.image.load('./Images/Runing Animation/image2.png'), (200, 200)))
+        self.sprites = {"right": [], "left": []}
+        for i in range(1, 3):
+            self.sprites["right"].append(pygame.transform.scale(pygame.image.load(f'./Images/Runing Animation/image ({i}).png'), (200, 200)))
+        for i in range (3, 5):
+            self.sprites["left"].append(pygame.transform.scale(pygame.image.load(f'./Images/Runing Animation/image ({i}).png'), (200, 200)))
         self.current_sprite = 0
-        self.image = self.sprites[self.current_sprite]
+        self.image = self.sprites["right"][self.current_sprite]
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.pos_y_inicial = pos_y
         self.rect = self.image.get_rect()
         self.rect.topleft = [self.pos_x, self.pos_y]
         self.jumping = False
-        self.wallking = False
+        self.walking_right = False
+        self.walking_left = False
+        # self.walking_left_animation = False
+        # self.walking_right_animation = False
+        self.animation = self.Animation(self)
 
     def update(self):
 
@@ -34,24 +40,55 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rect.y = self.pos_y_inicial
 
+        if self.walking_left:
+            self.rect.x -= 5
+            self.animation.walking_left()
+        
+        if self.walking_right:
+            self.rect.x += 5
+            self.animation.walking_right()
 
-        if self.wallking:
-
-            self.current_sprite += 0.1
-
-            if self.current_sprite >= len(self.sprites):
-                self.current_sprite = 0
-
-            self.image = self.sprites[int(self.current_sprite)]
 
     def jump(self):
         self.jumping = True
 
-    def wallk(self):
-        self.wallking = True
+    def walk_right(self):
+        self.stop_walk_lef()
+        self.walking_right = True
 
-    def stop(self):
-        self.wallking = False
+    def walk_left(self):
+        self.stop_walk_right()
+        self.walking_left = True
+
+    def stop_walk_right(self):
+        self.walking_right = False
+
+    def stop_walk_lef(self):
+        self.walking_left = False
+
+
+    class Animation():
+
+        def __init__(self, parent):
+            self.parent = parent
+
+        def walking_right(self):
+
+            self.parent.current_sprite += 0.2
+
+            if self.parent.current_sprite > len(self.parent.sprites["right"]):
+                self.parent.current_sprite = 0
+
+            self.parent.image = self.parent.sprites["right"][int(self.parent.current_sprite)]
+
+        def walking_left(self):
+
+            self.parent.current_sprite += 0.2
+
+            if self.parent.current_sprite > len(self.parent.sprites["left"]):
+                self.parent.current_sprite = 0
+
+            self.parent.image = self.parent.sprites["left"][int(self.parent.current_sprite)]
 
 
 class Background(pygame.sprite.Sprite):
@@ -77,41 +114,37 @@ class Background(pygame.sprite.Sprite):
         
         if self.wallking_left:
 
+            self.current_sprite -= 1
+
+            if self.current_sprite < 0:
+                self.current_sprite = 299
+
+            self.image = self.sprites[int(self.current_sprite)]
+
+        elif self.wallking_right:
+
             self.current_sprite += 1
-
-
-            print("right",self.current_sprite)
 
             if self.current_sprite >= len(self.sprites):
                 self.current_sprite = 0
 
             self.image = self.sprites[int(self.current_sprite)]
-        elif self.wallking_right:
-
-            self.current_sprite -= 1
-
-            if self.current_sprite < 0:
-
-                print(len(self.sprites))
-                self.current_sprite = 299
-
-            self.image = self.sprites[int(self.current_sprite)]
      
     def move_left(self):
-        # self.rect.x += steps
+        self.wallking_right = False
         self.wallking_left = True
 
     def move_right(self):
-        # self.rect.x += steps
+        self.wallking_left = False
         self.wallking_right = True
 
 
-    def stop(self):
-
-        self.wallking_left = False
+    def stop_move_right(self):
         self.wallking_right = False
+    
+    def stop_move_left(self):
+        self.wallking_left = False
 
-        
 
 ## Defini o tamanho da tela
 screen_width = 768
@@ -122,7 +155,7 @@ pygame.display.set_caption('Socioly Game')
 
 background_image = Background(0, 0)
 moving_sprites = pygame.sprite.Group()
-player = Player(0, 230)
+player = Player(50, 230)
 
 moving_sprites.add(background_image)
 moving_sprites.add(player)
@@ -143,21 +176,32 @@ while True:
         
     keys = pygame.key.get_pressed()
 
-    if any(keys):
-        player.wallk()
-    else:
-        player.stop()
+
+    # print(player.rect.x >= (screen_width - 250) or player.rect.x <= 40)
 
     if keys[K_d]:
-        background_image.move_left()
-
-    elif keys[K_a]:
-        background_image.move_right()
+        if(player.rect.x >= (screen_width - 250)):
+            player.stop_walk_right()
+            player.animation.walking_right()
+            background_image.move_right()
+        else:
+            player.walk_right()
+    else:
+        background_image.stop_move_right()
+        player.stop_walk_right()
     
-    if not keys[K_d] and not keys[K_a]:
-        background_image.stop()
-
-
+    if keys[K_a]:
+        if(player.rect.x <= 40):
+            player.stop_walk_lef()
+            player.animation.walking_left()
+            background_image.move_left()
+        else:
+            player.walk_left()
+    else:
+        background_image.stop_move_left()
+        player.stop_walk_lef()
+        
+    
     moving_sprites.update()
     moving_sprites.draw(screen)
 
